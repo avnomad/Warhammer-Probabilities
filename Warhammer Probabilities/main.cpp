ece65a047ecb4f53e6843720f55f3453890797cd
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-03-31 18:38:16 +0300

    Modified code to compute possibility to win now only once for each (n1,n2) pair.
    Results are stored in baseCases 2-dimentional vector.

--- Warhammer Probabilities/main.cpp
+++ Warhammer Probabilities/main.cpp
@@ -6,6 +6,9 @@ using std::endl;
 #include <cmath>
 using std::pow;
 
+#include <vector>
+using std::vector;
+
 #include <Math/mathematical functions.h>
 using Math::MathematicalFunctions::BernsteinTriangle;
 
@@ -39,16 +42,37 @@ int main()
 
 BernsteinTriangle *triangle1;
 BernsteinTriangle *triangle2;
+vector<vector<double>> baseCases;	// will hold the possibility to win now for each (n1,n2) pair.
 
-double determine_win_posibility(double p1, double p2, int n1, int n2, int turns)
+double determine_win_posibility(double p1, double p2, int nModels1, int nModels2, int turns)	// not thread safe!
 {
-	BernsteinTriangle bernsteinTriangle1(p1,n1);
-	BernsteinTriangle bernsteinTriangle2(p2,n2);
+	BernsteinTriangle bernsteinValues1(p1,nModels1);
+	BernsteinTriangle bernsteinValues2(p2,nModels2);
 
-	triangle1 = &bernsteinTriangle1;
-	triangle2 = &bernsteinTriangle2;
+	triangle1 = &bernsteinValues1;
+	triangle2 = &bernsteinValues2;
 
-	return determine_win_posibility_worker(n1,n2,turns);
+	if(nModels1 == 0) return 0.0;	// if there are no attackers, attackers can't win.
+	if(nModels2 == 0) return 1.0;	// if there are no defenders, attackers can't loose.
+		
+	// precompute base case determine_win_posibility_worker(n1,n2,1) for each (n1,n2) pair.
+	baseCases.resize(nModels1+1);
+	for(auto n1 = 1u ; n1 < baseCases.size() ; n1++)	// we will never try to access baseCases[0][x] nor baseCases[x][0]
+	{													// should probably save the memory too..
+		baseCases[n1].resize(nModels2+1);				// should also verify the proof...
+		for(auto n2 = 1u ; n2 < baseCases[n1].size() ; n2++)
+		{
+			// calculate possibility to win in current turn
+			double sum = 0;
+			for(auto i = n2 ; i<=n1 ; i++)	// at least enough attacks to kill all enemy models need to succeed.
+				for(auto j = 0u; j<n1 && j<=n2 ; j++)	// a least 1 model of 1st unit must survive.
+	    			sum += bernsteinValues1(n1,i)	// possibility exactly i unit 1 attacks succeed...
+						* bernsteinValues2(n2,j);	// ...and exactly j unit 2 attacks succeed.
+			baseCases[n1][n2] = sum;
+		} // end for
+	} // end for
+
+	return determine_win_posibility_worker(nModels1,nModels2,turns);
 } // end function determine_win_posibility
 
 
@@ -69,9 +93,7 @@ double determine_win_posibility_worker(int n1, int n2, int turns)
 					* determine_win_posibility_worker(n1-j, n2-i, turns-1);	// ...and unit 1 defeats unit 2 later but not before game ends.
 	} // end if
 	// ... + possibility to win now
-	for(i = n2 ; i<=n1 ; i++)	// at least enough attacks to kill all enemy models need to succeed.
-		for(j = 0; j<n1 && j<=n2 ; j++)	// a least 1 model of 1st unit must survive.
-	    	sum += (*triangle1)(n1,i)	// possibility exactly i unit 1 attacks succeed...
-				* (*triangle2)(n2,j);	// ...and exactly j unit 2 attacks succeed.
+	sum += baseCases[n1][n2];
+
 	return sum;
 } // end function determine_win_posibility_worker
